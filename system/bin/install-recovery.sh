6b7228b8357f7525be07d343354c1ee1ec699e85
= mlm-games <= gerrit.r6a6a@passinbox.com>
2024-09-17 21:36:31 +0530

    Init

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/bootdevice/by-name/recovery:11244448:7b75042607cfe3f3daa8a85e2ec457221639b713; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/bootdevice/by-name/boot:9864096:92ce37f298ef72f57cc53145b9b4b1e29f323b5c EMMC:/dev/block/platform/bootdevice/by-name/recovery 7b75042607cfe3f3daa8a85e2ec457221639b713 11244448 92ce37f298ef72f57cc53145b9b4b1e29f323b5c:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/bootdevice/by-name/recovery:11240352:a719e27477321baee3d87d39638f79b7dedb9283; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/bootdevice/by-name/boot:9860000:5e6a669796b33deb7290f22a1131d590f1cea12f EMMC:/dev/block/platform/bootdevice/by-name/recovery a719e27477321baee3d87d39638f79b7dedb9283 11240352 5e6a669796b33deb7290f22a1131d590f1cea12f:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
